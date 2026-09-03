47ddb02e8e75c74a0ae6b161992c2fb0cd64df71
Shashidhar Reddy <shashirdy74@gmail.com>
2026-09-03 13:02:59 -0400

    Create ClaimsProcessorApplication.java

---
+++ src/main/java/com/shashireddy/claims/ClaimsProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.shashireddy.claims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsProcessorApplication.class, args);
+    }
+}
